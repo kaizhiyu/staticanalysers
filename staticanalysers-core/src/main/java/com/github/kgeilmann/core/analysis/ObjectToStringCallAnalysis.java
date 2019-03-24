8a06ed9d2703f3ac91e97e000f852288cdb1e09c
Kathrin Geilmann <k>
2019-03-24 17:01:09 +0100

    Finished first version of ObjectToStringCall analysis. The results look ok for the first version, but theres still room to improve.

--- staticanalysers-core/src/main/java/com/github/kgeilmann/core/analysis/ObjectToStringCallAnalysis.java
+++ staticanalysers-core/src/main/java/com/github/kgeilmann/core/analysis/ObjectToStringCallAnalysis.java
@@ -33,7 +33,8 @@ public class ObjectToStringCallAnalysis extends Analysis {
             "org.apache.log4j.Category.error(java.lang.Object, java.lang.Throwable)",
             "org.apache.log4j.Category.fatal(java.lang.Object, java.lang.Throwable)"
     );
-    private static final String MESSAGE_EXPLICIT = "Call of toString() might be executed on java.lang.Object";
+    private static final String MESSAGE_EXPLICIT = "Call of toString() might be executed on java.lang.Object.";
+    private static final String MESSAGE_IMPLICIT = "Implicit call of toString() might be executed on java.lang.Object.";
 
     @Override
     public String getDescription() {
@@ -88,52 +89,96 @@ public class ObjectToStringCallAnalysis extends Analysis {
 
         @Override
         public Optional<AnalysisResult> visit(BinaryExpr expr, String arg) {
-            return super.visit(expr, arg);
+            if (!expr.getOperator().equals(BinaryExpr.Operator.PLUS)) {
+                return super.visit(expr, arg);
+            }
+
+            if (!"java.lang.String".equals(expr.calculateResolvedType().describe())) {
+                return super.visit(expr, arg);
+            }
+
+            boolean leftIsString = "java.lang.String".equals(expr.getLeft().calculateResolvedType().describe());
+            boolean rightIsString = "java.lang.String".equals(expr.getRight().calculateResolvedType().describe());
+
+            if (leftIsString && rightIsString) {
+                // no string conversion takes place -> no implicit toString call
+                return super.visit(expr, arg);
+            }
+
+            if (!leftIsString && isAcceptedImplicitCall(expr.getLeft())) {
+                return super.visit(expr, arg);
+            }
+
+            if (!rightIsString && isAcceptedImplicitCall(expr.getRight())) {
+                return super.visit(expr, arg);
+            }
+
+            if (!leftIsString && !rightIsString) {
+                // calculateResolvedType did return string, but its not, because string conversion on both sides needed -> Bug in Symbolsolver
+                throw new IllegalArgumentException("Something is wrong with binary expressions and string conversions.");
+            }
+
+            return result(expr, MESSAGE_IMPLICIT, arg);
         }
 
         @Override
         public Optional<AnalysisResult> visit(MethodCallExpr mc, String surroundingType) {
-            if (!"toString".equals(mc.getName().asString())) {
-                return super.visit(mc, surroundingType);
-            }
-
             try {
-                ResolvedMethodDeclaration resolved = mc.resolve();
-                if (!("java.lang.Object.toString()".equals(resolved.getQualifiedSignature()))) {
+                if (!"toString".equals(mc.getName().asString())) {
                     return super.visit(mc, surroundingType);
                 }
+
+                if (isAcceptedCall(mc)) {
+                    return super.visit(mc, surroundingType);
+                }
+
+                return result(mc, MESSAGE_EXPLICIT, surroundingType);
             } catch (UnsolvedSymbolException e) {
                 return result(mc, UNSOLVED, surroundingType);
+            }
+        }
+
+        private boolean isAcceptedImplicitCall(Expression exp) {
+            // change the ast to contain an explicit call
+            MethodCallExpr call = new MethodCallExpr();
+            exp.replace(call);
+            call.setScope(exp);
+            call.setName("toString");
+
+            boolean accepted = isAcceptedCall(call);
+
+            // rollback the change in ast
+            call.replace(exp);
+            return accepted;
+        }
+
+        private boolean isAcceptedCall(MethodCallExpr mc) {
+            ResolvedMethodDeclaration toStringMethod = mc.resolve();
+            if (!("java.lang.Object.toString()".equals(toStringMethod.getQualifiedSignature()))) {
+                return true;
             }
 
             // It's really a call to Object.toString(), which we don't want to have at runtime.
             // But flagging all found calls as error, would result is a lot of false positives, so let's accept some of these calls
 
             // Most interfaces don't declare toString() explicitly and we will accept that
-            if (isCallOnInterfaceType(mc)) {
-                return super.visit(mc, surroundingType);
-            }
-
-            return result(mc, MESSAGE_EXPLICIT, surroundingType);
+            return isCallOnInterfaceType(mc);
         }
 
         private boolean isCallOnInterfaceType(MethodCallExpr mc) {
-            ResolvedType typeOfScope = mc.getScope().map(exp -> exp.calculateResolvedType()).orElseThrow(() -> new IllegalArgumentException("Unexpected type of scope"));
+
+            ResolvedType typeOfScope = mc.getScope()
+                    .map(Expression::calculateResolvedType).orElseThrow(() -> new IllegalArgumentException("FIXME: No scope " + mc.toString()));
             if (typeOfScope.isReferenceType()) {
                 ResolvedReferenceTypeDeclaration typeDecl = typeOfScope.asReferenceType().getTypeDeclaration();
-                if (typeDecl.isInterface()) {
-                    // TODO: make analysis more meaningful, by assuming closed world and checking that all classes implementing interface have correct toString method
-                    return true;
-                } else {
-                    return false;
-                }
+                // TODO: make analysis more meaningful, by assuming closed world and checking that all classes implementing interface have correct toString method
+                return typeDecl.isInterface();
             } else if (typeOfScope.isTypeVariable()) {
                 // FIXME: handle by check on type bound?
-                throw new IllegalArgumentException("Unexpected type of scope: Type variable: how to solve?");
-            } else {
-                // FIXME: throw instead of default value to find out, what has been missed
-                throw new IllegalArgumentException("Unexpected type of scope");
+                throw new IllegalArgumentException("FIXME: scope is type variable " + mc.toString());
             }
+
+            return false;
         }
     }
 }
